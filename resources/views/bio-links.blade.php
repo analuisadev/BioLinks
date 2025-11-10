59fdfd25d8cb2a92e7d99d63e689d46ca3cbb922
analuisadev <analuisavidal89@gmail.com>
2025-11-10 17:09:48 -0300

    feat: Tela pública de links

---
+++ resources/views/bio-links.blade.php
@@ -0,0 +1,16 @@
+<div>
+    <img src="/storage/{{  $user->photo }}" alt="Foto de perfil de usuário" />
+
+    <h2>Usuário {{  $user->name  }}</h2>
+    <p>{{  $user->description  }}</p>
+
+    <ul>
+        @foreach ($user->links as $link)
+            <li>
+
+                <a href="{{ $link->link }}" target="_blank">{{  $link->id  }}.{{ $link->name }}</a>
+
+            </li>
+        @endforeach
+    </ul>
+</div>
